589453c87c23f3c62fa359466964cc80f634e66a
Diego Garcia Padilla <gpdiego370@outlook.com>
2023-06-01 10:21:47 -0600

    Implement polymorphism in the Board class

--- headers/Board.h
+++ headers/Board.h
@@ -28,19 +28,16 @@ class Board
 {
 private:
     // Attributes
-    std::vector<std::vector<Piece>> board;
-    int turn;   // 0 = white, 1 = black
+    Piece* board[8][8];
+    int turn = 0;   // 0 = white, 1 = black
 public:
     // Constructor
     Board();
-    Board(std::vector<std::vector<Piece>> board, int turn);
 
     // Getters
-    std::vector<std::vector<Piece>> getBoard();
     int getTurn();
 
     // Setters
-    void setBoard(std::vector<std::vector<Piece>> board);
     void setTurn(int turn);
 
     // Methods
@@ -58,35 +55,20 @@ public:
 
 Board::Board()
 {
-    board = std::vector<std::vector<Piece>>(8, std::vector<Piece>(8));
+    // Initialize the board with empty spaces
+    for (int i = 2; i < 6; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            board[i][j] = new Piece();
+        }
+    }
+
+    // Set the turn to white
     turn = 0;
 }
 
-/**
- * @brief Construct a new Board:: Board object
- * 
- * @param board 
- * @param turn 
- */
-
-Board::Board(std::vector<std::vector<Piece>> board, int turn)
-{
-    this->board = board;
-    this->turn = turn;
-}
-
 // Getters
-
-/**
- * @brief Get the Board object
- * 
- * @return std::vector<std::vector<Piece>> 
- */
-
-std::vector<std::vector<Piece>> Board::getBoard()
-{
-    return board;
-}
 
 /**
  * @brief Get the Turn object
@@ -102,17 +84,6 @@ int Board::getTurn()
 // Setters
 
 /**
- * @brief Set the Board object
- * 
- * @param board 
- */
-
-void Board::setBoard(std::vector<std::vector<Piece>> board)
-{
-    this->board = board;
-}
-
-/**
  * @brief Set the Turn object
  * 
  * @param turn 
@@ -126,7 +97,7 @@ void Board::setTurn(int turn)
 // Methods
 
 /**
- * @brief Initializes the board with the pieces in their initial positions
+ * @brief Initialize the board with the pieces in their initial positions
  * 
  */
 
@@ -137,66 +108,68 @@ void Board::initializeBoard()
     {
         for (int j = 0; j < 8; j++)
         {
-            board[i][j] = Piece();
+            board[i][j] = new Piece();
         }
     }
 
-
-    // White pieces
-    
-    // Pawns
-    for (int i = 0; i < 8; i++)
-    {
-        board[1][i] = Pawn(0, std::make_pair(1, i));
-    }
-
-    // Rooks
-    board[0][0] = Rook(0, std::make_pair(0, 0));
-    board[0][7] = Rook(0, std::make_pair(0, 7));
-
-    // Knights
-    board[0][1] = Knight(0, std::make_pair(0, 1));
-    board[0][6] = Knight(0, std::make_pair(0, 6));
-
-    // Bishops
-    board[0][2] = Bishop(0, std::make_pair(0, 2));
-    board[0][5] = Bishop(0, std::make_pair(0, 5));
-
-    // Queen
-    board[0][3] = Queen(0, std::make_pair(0, 3));
-
-    // King
-    board[0][4] = King(0, std::make_pair(0, 4));
-
-    // Black pieces
+    // Initialize the white pieces
 
     // Pawns
     for (int i = 0; i < 8; i++)
     {
-        board[6][i] = Pawn(1, std::make_pair(6, i));
+        board[1][i] = new Pawn(0, std::make_pair(1, i));
     }
 
     // Rooks
-    board[7][0] = Rook(1, std::make_pair(7, 0));
-    board[7][7] = Rook(1, std::make_pair(7, 7));
+    board[0][0] = new Rook(0, std::make_pair(0, 0));
+    board[0][7] = new Rook(0, std::make_pair(0, 7));
 
     // Knights
-    board[7][1] = Knight(1, std::make_pair(7, 1));
-    board[7][6] = Knight(1, std::make_pair(7, 6));
+    board[0][1] = new Knight(0, std::make_pair(0, 1));
+    board[0][6] = new Knight(0, std::make_pair(0, 6));
 
     // Bishops
-    board[7][2] = Bishop(1, std::make_pair(7, 2));
-    board[7][5] = Bishop(1, std::make_pair(7, 5));
+    board[0][2] = new Bishop(0, std::make_pair(0, 2));
+    board[0][5] = new Bishop(0, std::make_pair(0, 5));
 
     // Queen
-    board[7][3] = Queen(1, std::make_pair(7, 3));
+    board[0][3] = new Queen(0, std::make_pair(0, 3));
 
     // King
-    board[7][4] = King(1, std::make_pair(7, 4));
+    board[0][4] = new King(0, std::make_pair(0, 4));
+
+    // Initialize the black pieces
+
+    // Pawns
+
+    for (int i = 0; i < 8; i++)
+    {
+        board[6][i] = new Pawn(1, std::make_pair(6, i));
+    }
+
+    // Rooks
+
+    board[7][0] = new Rook(1, std::make_pair(7, 0));
+    board[7][7] = new Rook(1, std::make_pair(7, 7));
+
+    // Knights
+
+    board[7][1] = new Knight(1, std::make_pair(7, 1));
+    board[7][6] = new Knight(1, std::make_pair(7, 6));
+
+    // Bishops
+    board[7][2] = new Bishop(1, std::make_pair(7, 2));
+    board[7][5] = new Bishop(1, std::make_pair(7, 5));
+
+    // Queen
+    board[7][3] = new Queen(1, std::make_pair(7, 3));
+
+    // King
+    board[7][4] = new King(1, std::make_pair(7, 4));
 }
 
 /**
- * @brief Prints the board
+ * @brief Print the board
  * 
  */
 
@@ -214,7 +187,7 @@ void Board::printBoard()
         std::cout << i << " ";
         for (int j = 0; j < 8; j++)
         {
-            std::cout << board[i][j].getSymbol() << " ";
+            std::cout << board[i][j]->getSymbol() << " ";
         }
         std::cout << std::endl;
     }
